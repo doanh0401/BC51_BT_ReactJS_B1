3cfaa1a799c237b42b7c1ec4d614f35f92582194
doanh0401 <doanhkhai.busted@gmail.com>
2023-08-07 19:13:54 +0700

    .

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import BaiTapThucHanhLayOut from './BaiTapThucHanhLayOut/BaiTapThucHanhLayOut';
+
+function App() {
+  return (
+    <BaiTapThucHanhLayOut />
+  );
+}
+
+export default App;
